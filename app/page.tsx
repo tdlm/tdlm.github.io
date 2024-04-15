a84c6df0ebabcfe9939d3ce94415626edc62aac2
Scott Weaver <scottmweaver@gmail.com>
2024-04-15 15:44:43 -0700

    Remove "out" folder

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      TDLM
-      <Link href="/test">Test</Link>
+    <main className="">
+
     </main>
   );
 }
